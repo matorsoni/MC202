10871a12d241177505a43fa7c9f2684c556681cd
matorsoni <matorsoni45@gmail.com>
2019-10-05 12:09:16 -0300

    lab04 começando

---
+++ lab04/lab04.c
@@ -0,0 +1,16 @@
+/*
+Aluno: Mateus Orsoni Cabral
+RA: 147349
+Disciplina: MC202 turma H
+Os nomes de variáveis e de funções são eventualmente escritos em inglês apenas por uma questão de costume. 
+Os comentários ao longo do código serão sempre em português.
+*/
+
+#include "lista.h"
+
+int main()
+{
+    printf("NULL = %d\n", NULL);
+    return 0;
+
+}
---
+++ lab04/lista.c
@@ -0,0 +1,9 @@
+/*
+Aluno: Mateus Orsoni Cabral
+RA: 147349
+Disciplina: MC202 turma H
+Os nomes de variáveis e de funções são eventualmente escritos em inglês apenas por uma questão de costume. 
+Os comentários ao longo do código serão sempre em português.
+*/
+
+#include "lista.h"
